c29b836e46e348e212e01ae0fccee79ee4350a75
Renato <rverissimo.dev@gmail.com>
2025-06-17 15:32:58 -0300

    feat: make a few adjustments to dockerfile

--- src/app.js
+++ src/app.js
@@ -6,3 +6,10 @@ app.get('/health', (req, res) => {
     res.status(200).send('Payment service is up and running');
 });
 
+async function startService(){
+    app.listen(3000, () => {
+        console.log('A payment service instance is running on port 3000');
+    });
+}
+
+startService();
