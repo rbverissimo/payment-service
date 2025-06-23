71e42a5c839e89840b10c5cba21e6e1f0e5d3fc8
Renato <rverissimo.dev@gmail.com>
2025-06-23 14:10:56 -0300

    feat: add post method that registers an payment

--- src/app.js
+++ src/app.js
@@ -10,6 +10,33 @@ app.get('/health', (req, res) => {
     res.status(200).send('Payment service is up and running');
 });
 
+app.post('/payments', async (req, res) => {
+    const { orderId, amount } = req.body;
+
+    if(!orderId || !amount){
+        return res.status(400).json({message: 'Order ID and amount are required.'});
+    }
+
+    try {
+        
+        const status = 'completed';
+        const transaction_id = `txn_${Date.now()}_${Math.random().toString(36).substring(2, 9)}`;
+
+        const result = await db.query(
+            'INSERT INTO payments(order_id, amount, status, transaction_id) VALUES ($1, $2, $3, $4) RETURNING *',
+            [orderId, amount, status, transaction_id]
+        )
+
+        const payment = result.rows[0];
+        console.log('New payment processed and saved into database: ', payment);
+
+        return res.status(202).json({message: 'Payment processed successfully', obj: payment});
+    } catch (error) {
+        
+    }
+
+});
+
 async function startService(){
     try {
 
