c06a185cae5e5214f2ef6da585582bdc1c2910ec
Renato <rverissimo.dev@gmail.com>
2025-07-24 14:18:14 -0300

    feat: add a domain layer with payment as the core layer of this service

---
+++ src/domain/Payment.js
@@ -0,0 +1,24 @@
+class Payment {
+ 
+    /**
+     * 
+     * @param {Object} data
+     * @param {number} data.id
+     * @param {string} data.orderId 
+     * @param {number} data.amount
+     * @param {string} data.status
+     * @param {string} data.transactionId
+     * @param {Date} data.createdAt 
+     */
+    constructor({id, orderId, amount, status, transactionId, createdAt}){
+        this.id = id;
+        this.orderId = orderId;
+        this.amount = amount;
+        this.status = status;
+        this.transactionId = transactionId;
+        this.createdAt = createdAt;
+    }
+
+}
+
+export default Payment;
